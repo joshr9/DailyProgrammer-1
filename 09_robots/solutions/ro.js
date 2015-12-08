a2617acc99393fb2d47598c982652497daad25bf
joshr9 <joshruchwarger@gmail.com>
2015-12-08 14:24:15 -0700

    started algorithm for problem

--- 09_robots/solutions/ro.js
+++ 09_robots/solutions/ro.js
@@ -1,1 +1,15 @@
 var challenge = require('../robots.js');
+var markerHit = false;
+
+
+while(!challenge.checkCollision()){
+  if(challenge.robotOne.onMarker()){
+    markerHit = true;
+  }else{
+    if(markerHit){
+      challenge.robotOne.moveTwo('left')
+    }else{
+      challenge.robotOne.moveOne('left')
+    }
+  }
+}
